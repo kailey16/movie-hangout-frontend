d5316f85551622fe6355c1af14484caa3d6f8b42
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2019-12-04 09:50:48 -0500

    Homepage fixed

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,13 +4,25 @@ import {Link} from 'react-router-dom'
 class Poster extends React.Component {
 
   render() {
-    return (
-      <div className="Poster">
-        {this.props.movie &&
-        <Link to={`/movies/${this.props.movie.id}`}><img className={this.props.classes} alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + this.props.movie["poster_path"]}/></Link>
-        } 
-      </div>
-    )
+
+    if (this.props.movie) {
+      if (this.props.movie["movieAPI_ID"]) {
+        return (
+          <div className="Poster">
+            <Link to={`/movies/${this.props.movie.movieAPI_ID}`}><img className={this.props.classes} alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + this.props.movie["poster_path"]}/></Link>
+          </div>
+        )
+      } else {
+        return (
+          <div className="Poster">
+            {this.props.movie && (
+            <Link to={`/movies/${this.props.movie.id}`}><img className={this.props.classes} alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + this.props.movie["poster_path"]}/></Link>)
+            } 
+          </div>
+        )
+      }
+    }
+
   }
 
 }
